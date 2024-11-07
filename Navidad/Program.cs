0b4d22ce929f3a43a37a3d9850ae89d4e9f4cb94
Daniel . <sngelaneagu5620@gmail.com>
2024-11-07 14:38:29 +0100

    test

--- Navidad/Program.cs
+++ Navidad/Program.cs
@@ -1,15 +1,36 @@
-﻿class arbol
+﻿class Arbol
 {
     public static void Main(string[] args)
     {
-        for (int i = 0; i < 6; i++)
-        {
-            Console.Write("\n");
+        int width;
+        int height;
 
-            for (int j = 0; j < 7; j++)
+        Console.WriteLine("How width do you want your tree?");
+        width = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("How height do you want your tree?");
+        height = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("      0");
+        for (int i = 1; i <= 6; i++)
+        {
+            for (int l = 5; l >= i; l--)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 1; k <= i; k++)
             {
                 Console.Write("*");
             }
+            Console.Write(i);
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        for (int ñ = 0; ñ < 2; ñ++)
+        {
+            Console.WriteLine("     |||");
         }
     }
 }
