d6cc4d7864746eb9f157ac7c79b451c6e3835cc3
Daniel . <8danidd@gmail.com>
2024-11-07 13:28:35 +0100

    Agregar archivos de proyecto.

---
+++ Navidad/Program.cs
@@ -0,0 +1,15 @@
+﻿class arbol
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            Console.Write("\n");
+
+            for (int j = 0; j < 7; j++)
+            {
+                Console.Write("*");
+            }
+        }
+    }
+}
